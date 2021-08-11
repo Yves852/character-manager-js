46c58f7fea1d5bff59eb4858f70ce7395f0912c3
thibaut3005 <Vous@exemple.com>
2021-08-11 13:27:36 +0200

    post

---
+++ post.js
@@ -0,0 +1,28 @@
+let inputpseudo = document.getElementById('editor-character-name');
+let inputshortdesc =document.getElementById('editor-character-short')
+let inputdesc =document.getElementById('editor-character-description');
+let pseudo = ""
+let shortdesc = ""
+let desc = "" 
+
+
+inputpseudo.addEventListener("input", (e) =>{
+    pseudo= e.target.value; 
+})
+inputshortdesc.addEventListener("input", (e) =>{
+    short = e.target.value;
+})
+inputdesc.addEventListener("input", (e) =>{
+    desc = e.target.value
+})
+
+
+
+
+
+le
+
+
+
+
+
