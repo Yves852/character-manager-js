ecc86433e2b1ce9f1de5bbab121ea51ea1047ac7
Yves Thirion <thirion.yves@gmail.com>
2021-08-12 11:23:30 +0200

    Created base for PUT, from POST

---
+++ put.js
@@ -0,0 +1,47 @@
+const inputs = Array.from(document.querySelectorAll("input"));
+
+// When called from Update character, fill data from selected character and http method is PUT
+
+const preparePut = async id => {
+    // Refresh data from the character and fill inputs with its data
+    const result = await fetch(`http://localhost:3000/character${id}`,{ method: "POST" });
+    const character = await result.json();
+    inputs.forEach(input => {
+        switch(input.id){
+            case "editor-character-name":
+                input.value = character.name;
+                break;
+            case "editor-character-short":
+                input.value = character.shortDescription;
+                break;
+            case "editor-character-description":
+                input.value = character.description;
+                break;
+            // TODO image
+        }
+    });
+
+    document.getElementById('save').addEventListener('click', async () => {        
+        const values = inputs.map(({value}) => value.trim());
+        // Check if any input is empty
+        if (values.some((value) => value === "")){
+            console.log(`you must fill all the forms!`);
+            return;
+        }
+        const [image,name,shortdesc,description] = values;
+        const response = await fetch(`http://localhost:3000/character${id}`,{
+            method: "PUT",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body: JSON.stringify({
+                description,
+                shortdesc,
+                name,
+                image,
+            })
+        });
+        const newcharacter = await response.json();
+        console.log(newcharacter);
+    });
+}
