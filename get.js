c758c6bf3130ef0a46727284e4e73caa67124235
Yves Thirion <thirion.yves@gmail.com>
2021-08-12 17:25:30 +0200

    Get can adapt display if list of cards or one
    Check the url to find an id of character, if one use it to get the character else get the list. Adapt display to the list page or the card of character page

--- get.js
+++ get.js
@@ -17,21 +17,35 @@ const getCharacter = async id => {
 }
 /* Function creating cards from template and filling with data */
 const displayCharacters = async () => {
-    const characters = await getCharacter();
+    // Check if an id is sent in url and use it on getCharacter
+    const searchParams = new URLSearchParams(document.URL);
+    const idUrl = searchParams.get("file:///home/yves/Documents/Travail/Becode - webdev/Projects/character-manager-js/index.html?id");
+    const characters = idUrl ? await getCharacter(idUrl) : await getCharacter();
     const target = document.querySelector(".cardPool");
     const template = document.getElementById("template");
-    if(characters.length > 0){
+    // If receive an array of multiple characters, loop on it and display and prepare button
+    // Else display the unique character card with adapted style
+    if( characters[0] != undefined && characters.length > 0){
         characters.forEach((character) => {
             let clone = template.content.cloneNode(true);
-            // TODO open character page
             clone.querySelector(".card__button").addEventListener("click", async ()=>{
                 let char = await getCharacter(character.id);
-                console.log({name: char.name, descr: char.shortDescription});
+                window.open(`index.html?id=${character.id}`, '_blank');
             });
             clone.querySelector(".card__h3").innerHTML = character.name;
             clone.querySelector(".card__p").innerHTML = character.shortDescription;
             target.appendChild(clone);
         });
+    }
+    else {
+        let clone = template.content.cloneNode(true);
+        clone.querySelector(".card__h3").innerHTML = characters.name;
+        clone.querySelectorAll(".card__p")[0].innerHTML = characters.shortDescription;
+        clone.querySelectorAll(".card__p")[1].innerHTML = characters.description;
+        target.appendChild(clone);
+        target.children[1].classList.remove("card--pool");
+        target.children[1].parentElement.classList.remove("cardPool");
+        document.body.removeChild(document.getElementsByClassName("card__button--addnew")[0]);
     }
 }
 
