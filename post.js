0030412dce81a994684d9d07b183d51d9080c029
thibaut3005 <Vous@exemple.com>
2021-08-13 11:31:02 +0200

    tibo

--- post.js
+++ post.js
@@ -1,25 +1,38 @@
-const inputs = Array.from(document.querySelectorAll("input"));
+let  base64 = ""
 function previewFile() {
+    
     const preview = document.querySelector('img');
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
   
     reader.addEventListener("load", function () {
       // convert image file to base64 string
+      
       preview.src = reader.result;
+      base64 = reader.result
+    
+      console.log(base64);
     }, false);
   
     if (file) {
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(file); 
+    
     }
+
   }
 
 
 
 
-(() =>{
+
     document.getElementById('save').addEventListener('click', async() =>{
+        
+        
+        const inputs = Array.from(document.querySelectorAll(".editor-input"));
+        
         const values = inputs.map(({value}) => value.trim())
+        
+        
         
 
         if (values.some((value) => value === "")){
@@ -27,6 +40,9 @@ function previewFile() {
             return;
         }
         const [image,name,shortdesc,description] =values;
+
+
+
         const response = await fetch('http://localhost:3000/character',{
             method: "POST",
             headers:{
@@ -44,6 +60,6 @@ function previewFile() {
         console.log(newcharacter);
 
     });
-})();
+
 
 
