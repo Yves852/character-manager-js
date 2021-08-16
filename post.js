2c16ea85543eb1cda15ca5999d4e8ff89b3af3ba
Yves Thirion <thirion.yves@gmail.com>
2021-08-16 11:23:24 +0200

    Removed empty lines

--- post.js
+++ post.js
@@ -15,51 +15,31 @@ function previewFile() {
     }, false);
   
     if (file) {
-      reader.readAsDataURL(file); 
-    
+      reader.readAsDataURL(file);     
     }
-
   }
 
 
-
-
-
-    document.getElementById('save').addEventListener('click', async() =>{
-        
-        
-        const inputs = Array.from(document.querySelectorAll(".editor-input"));
-        
-        const values = inputs.map(({value}) => value.trim())
-        
-        
-        
-
-        if (values.some((value) => value === "")){
-            console.log(`you must fill all the forms!`);
-            return;
-        }
-        const [image,name,shortdesc,description] =values;
-
-
-
-        const response = await fetch('http://localhost:3000/character',{
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json",
-            },
-            body: JSON.stringify({
-                description,
-                shortdesc,
-                name,
-                image,
-            })
-        });
-        const newcharacter = await response.json
-
-        console.log(newcharacter);
-
+document.getElementById('save').addEventListener('click', async() =>{
+    const inputs = Array.from(document.querySelectorAll(".editor-input"));        
+    const values = inputs.map(({value}) => value.trim())
+    if (values.some((value) => value === "")){
+        console.log(`you must fill all the forms!`);
+        return;
+    }
+    const [image,name,shortdesc,description] =values;
+    const response = await fetch('http://localhost:3000/character',{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify({
+            description,
+            shortdesc,
+            name,
+            image,
+        })
     });
-
-
-
+    const newcharacter = await response.json
+    console.log(newcharacter);
+});
