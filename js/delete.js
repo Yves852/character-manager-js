23a5374c24df07fdcdc26670dd25c5b301f224de
thibaut3005 <thibautgregoire3005@gmail.com>
2021-08-17 12:16:40 +0200

    upload img

--- js/delete.js
+++ js/delete.js
@@ -1,11 +1,11 @@
- const del = () => {
+ const del = async id => {
      const r = confirm('Etes vous sur de vouloir supprimer ce personnage?');
             if (r == true){
-                const del = await fetch(`https://character-database.becode.xyz/characters/${idUrl}`, {method: 'DELETE',
+                const del = await fetch(`https://character-database.becode.xyz/characters/${id}`, {method: 'DELETE',
             headers:{
                 "Content-Type":"application/json",
                 
             }})
-            window.open('http://127.0.0.1:5500/index.html', '_self')
+            window.open('index.html', '_self')
             }else return 
         }
