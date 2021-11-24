cdf9d4429e1918842b6f258db90a6356a0066fe9
Yves <thirion.yves@gmail.com>
2021-11-24 16:23:39 +0100

    Prettier changes

--- js/delete.js
+++ js/delete.js
@@ -1,11 +1,15 @@
- const del = async id => {
-     const r = confirm('Etes vous sur de vouloir supprimer ce personnage?');
-            if (r == true){
-                const del = await fetch(`https://character-database.becode.xyz/characters/${id}`, {method: 'DELETE',
-            headers:{
-                "Content-Type":"application/json",
-                
-            }})
-            window.open('index.html', '_self')
-            }else return 
-        }
+const del = async (id) => {
+    const r = confirm("Etes vous sur de vouloir supprimer ce personnage?");
+    if (r) {
+        const del = await fetch(
+            `https://character-database.becode.xyz/characters/${id}`,
+            {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+        window.open("index.html", "_self");
+    } else return;
+};
