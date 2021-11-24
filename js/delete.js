d8b157dbd03e245d9be7998127ff17be9f73b7e8
Yves <thirion.yves@gmail.com>
2021-11-24 16:32:11 +0100

    Added trycatch for deletion request

--- js/delete.js
+++ js/delete.js
@@ -1,15 +1,20 @@
 const del = async (id) => {
     const r = confirm("Etes vous sur de vouloir supprimer ce personnage?");
     if (r) {
-        const del = await fetch(
-            `https://character-database.becode.xyz/characters/${id}`,
-            {
-                method: "DELETE",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }
-        );
-        window.open("index.html", "_self");
+        try {
+            const del = await fetch(
+                `https://character-database.becode.xyz/characters/${id}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            window.open("index.html", "_self");
+        } catch (error) {
+            // TODO replace console
+            console.log(error);
+        }
     } else return;
 };
